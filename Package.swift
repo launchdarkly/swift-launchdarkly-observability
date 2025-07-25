b6f1c0569fed64a366d238d206157876c7a43ed6
Mario Canto <mcanto@DYJ41XP7NT.local>
2025-07-25 17:03:33 -0600

    feat: instrumentation and session managers

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 6.1
+import PackageDescription
+
+let package = Package(
+    name: "swift-launchdarkly-observability",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "LaunchDarklyObservability",
+            targets: ["LaunchDarklyObservability"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/open-telemetry/opentelemetry-swift", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "LaunchDarklyObservability",
+            dependencies: [
+                "Shared",
+                .product(name: "OpenTelemetryApi", package: "opentelemetry-swift"),
+                .product(name: "OpenTelemetrySdk", package: "opentelemetry-swift"),
+                .product(name: "OpenTelemetryProtocolExporterHTTP", package: "opentelemetry-swift"),
+                .product(name: "StdoutExporter", package: "opentelemetry-swift"),
+                .product(name: "SignPostIntegration", package: "opentelemetry-swift"),
+            ]
+        ),
+        .testTarget(
+            name: "LaunchDarklyObservabilityTests",
+            dependencies: ["LaunchDarklyObservability"]
+        ),
+        .target(name: "Shared"),
+        .testTarget(
+            name: "SharedTests",
+            dependencies: ["Shared"]
+        )
+    ]
+)
