9cb9a52854520c7799b0552963812bfb9d3e79d6
mario-launchdarkly <mcanto@launchdarkly.com>
2025-09-04 09:09:49 -0600

    chore: organize targets

--- ObservabilityiOSTestApp/ObservabilityiOSTestApp/HomeOldView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct HomeOldView: View {
-    var body: some View {
-        Text("Old Home Experience")
-    }
-}
--- Sources/Client/SessionInfo.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-public struct SessionInfo: Sendable, Equatable {
-    public let id: String
-    public let startTime: Date
-}
