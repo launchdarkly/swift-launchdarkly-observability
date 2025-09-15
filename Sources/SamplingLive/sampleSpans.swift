3739759c239d2006d3c2084917886411bb95ccaf
mario-launchdarkly <mcanto@launchdarkly.com>
2025-09-15 13:05:40 -0600

    chore: address time complexity comments

--- Sources/SamplingLive/sampleSpans.swift
+++ Sources/SamplingLive/sampleSpans.swift
@@ -19,11 +19,7 @@ func sampleSpans(
     /// We also build a map of children spans by parent span id, which allows us to quickly traverse the span tree.
     for item in items {
         if let parentSpanId = item.parentSpanId {
-            if childrenByParentId[parentSpanId] == nil {
-                childrenByParentId[parentSpanId] = [item.spanId]
-            } else {
-                childrenByParentId[parentSpanId]?.append(item.spanId)
-            }
+            childrenByParentId[parentSpanId, default: []].append(item.spanId)
         }
         
         let sampleResult = sampler.sampleSpan(item)
@@ -40,8 +36,8 @@ func sampleSpans(
     
     /// Find all children of spans that have been sampled out and remove them.
     /// Repeat until there are no more children to remove.
-    while !omittedSpansIds.isEmpty {
-        let omittedSpanId = omittedSpansIds.removeFirst()
+    while let omittedSpanId = omittedSpansIds.popFirst() {
+        
         guard let affectedSpans = childrenByParentId[omittedSpanId] else { continue }
         
         for spanIdToRemove in affectedSpans {
@@ -50,11 +46,5 @@ func sampleSpans(
         }
     }
     
-    return Array(spanById.values).sorted { span1, span2 in
-        guard let index1 = items.firstIndex(of: span1),
-              let index2 = items.firstIndex(of: span2) else {
-            return false
-        }
-        return index1 < index2
-    }
+    return items.compactMap { spanById[$0.spanId] }
 }
