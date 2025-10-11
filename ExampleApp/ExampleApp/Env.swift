73127876ad8927598fdc9f1b4467172191f029f6
mario-launchdarkly <mcanto@launchdarkly.com>
2025-10-10 18:20:13 -0600

    chore: remove commented-out code

--- ExampleApp/ExampleApp/Env.swift
+++ ExampleApp/ExampleApp/Env.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 struct Env {
-    /// Provide 
     static var mobileKey: String {
         ProcessInfo.processInfo.environment["MOBILE_KEY"] ?? ""
     }
@@ -10,24 +9,3 @@ struct Env {
         ProcessInfo.processInfo.environment["OPTL_ENDPOINT"] ?? ""
     }
 }
-
-/*
-struct Env {
-    /// Provide
-    static var mobileKey: String {
-        guard let dict = Bundle.main.infoDictionary else {
-            return ""
-        }
-        
-        return dict["MOBILE_KEY"] as? String ?? ""
-    }
-    
-    static var otelHost: String {
-        guard let dict = Bundle.main.infoDictionary else {
-            return ""
-        }
-        
-        return dict["OPTL_ENDPOINT"] as? String ?? ""
-    }
-}
-*/
