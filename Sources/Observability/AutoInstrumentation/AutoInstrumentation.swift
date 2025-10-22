d5abb96060985c7228e4a87fd81a5ec015b5b26f
mario-launchdarkly <mcanto@launchdarkly.com>
2025-10-22 10:51:24 -0600

    chore: remove dead code, measurement task factory

--- Sources/Observability/AutoInstrumentation/AutoInstrumentation.swift
+++ Sources/Observability/AutoInstrumentation/AutoInstrumentation.swift
@@ -4,13 +4,3 @@ protocol AutoInstrumentation {
     func start()
     func stop()
 }
-
-struct MeasurementTaskFactory {
-    static func make(
-        metricsApi: MetricsApi,
-        samplingInterval: TimeInterval,
-        operation: @escaping (MetricsApi) async -> Void
-    ) -> AutoInstrumentation {
-        MeasurementTask(metricsApi: metricsApi, samplingInterval: samplingInterval, operation: operation)
-    }
-}
