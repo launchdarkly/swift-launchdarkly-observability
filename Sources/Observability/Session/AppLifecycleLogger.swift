667114046734abf367b7d12c13d7a4ae65795346
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-10-24 06:55:23 -0700

    Revert "delete logging stuff"
    This reverts commit 956834570e317c227450a340f393161eae19a96b.

---
+++ Sources/Observability/Session/AppLifecycleLogger.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+enum AppLifeCycleLogState: String, Sendable {
+    // The app has become active. Associated with UIKit notification applicationDidBecomeActive.
+    case active = "active"
+    // The app is now in the background. This value is associated with UIKit notification applicationDidEnterBackground.
+    case background = "background"
+    // The app is now in the foreground. This value is associated with UIKit notification applicationWillEnterForeground.
+    case foreground = "foreground"
+    // The app is now inactive. Associated with UIKit notification applicationWillResignActive.    Development
+    case inactive = "inactive"
+    // The app is about to terminate. Associated with UIKit notification applicationWillTerminate.
+    case terminate = "terminate"
+}
+
+public protocol AppLifecycleLogging {
+   
+}
+
+final class AppLifecycleLogger: AppLifecycleLogging {
+    let appLifecycleManager: AppLifecycleManaging
+    
+    init(appLifecycleManager: AppLifecycleManaging) {
+        self.appLifecycleManager = appLifecycleManager
+        
+        Task(priority: .background) { [weak self] in
+            guard let self else { return }
+            
+            for await event in await appLifecycleManager.events() {
+                
+            }
+        }
+    }
+}
