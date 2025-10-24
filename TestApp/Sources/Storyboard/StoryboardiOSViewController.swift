aa463725eb4a7c746a0bfe602830d8727653e96f
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-10-24 09:57:04 -0700

    Another unused fields

--- TestApp/Sources/Storyboard/StoryboardiOSViewController.swift
+++ TestApp/Sources/Storyboard/StoryboardiOSViewController.swift
@@ -1,10 +1,6 @@
 import UIKit
 
 class StoryboardiOSViewController: UIViewController {
-    // MARK: - Outlets
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var nameTextField: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
