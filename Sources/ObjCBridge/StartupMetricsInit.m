0732d1b82f570c7fa3d49eef4f4e3c34c778712a
mario-launchdarkly <mcanto@launchdarkly.com>
2025-11-03 17:26:37 -0600

    fix: stale commented swift code causing confusion

--- Sources/ObjCBridge/StartupMetricsInit.m
+++ Sources/ObjCBridge/StartupMetricsInit.m
@@ -5,10 +5,3 @@ __attribute__((constructor))
 static void StartupMetricsEarlyInit(void) {
     SwiftStartupMetricsInitialize();
 }
-
-//@_cdecl("SwiftStartupMetricsInitialize")
-//@_attribute__((constructor))
-//// Expose a function Swift can call from C
-//public func SwiftStartupMetricsInitialize() {
-//    _ = AppStartTime.stats
-//}
