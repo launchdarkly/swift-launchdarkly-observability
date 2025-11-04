787234aa27807e383544739221ef7094a41dcb6f
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-11-04 15:16:31 -0800

    cost calculation

--- Sources/Observability/Metrics/MetricItem.swift
+++ Sources/Observability/Metrics/MetricItem.swift
@@ -10,6 +10,6 @@ public struct MetricItem: EventQueueItemPayload {
     public var timestamp: TimeInterval
     
     public func cost() -> Int {
-        300 //+ metric.data.points.count * 100
+        300 + metricData.data.points.count * 100
     }
 }
