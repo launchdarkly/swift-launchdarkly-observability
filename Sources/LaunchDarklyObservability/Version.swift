eaa2bdc182cee5a984ff11f80c3569b3e0cfb01b
LaunchDarklyReleaseBot <86431345+LaunchDarklyReleaseBot@users.noreply.github.com>
2025-11-05 10:04:38 -0800

    chore(main): release 0.9.1 (#71)
    :robot: I have created a release *beep* *boop*
    ---
    
    
    ##
    [0.9.1](https://github.com/launchdarkly/swift-launchdarkly-observability/compare/0.9.0...0.9.1)
    (2025-11-05)
    
    
    ### Bug Fixes
    
    * Otlp Trace Exporters to Event Exporter
    ([#70](https://github.com/launchdarkly/swift-launchdarkly-observability/issues/70))
    ([cfa92fb](https://github.com/launchdarkly/swift-launchdarkly-observability/commit/cfa92fb851a4eea180ad70149a49505111916013))
    
    ---
    This PR was generated with [Release
    Please](https://github.com/googleapis/release-please). See
    [documentation](https://github.com/googleapis/release-please#release-please).
    
    <!-- CURSOR_SUMMARY -->
    ---
    
    > [!NOTE]
    > Bumps version to 0.9.1 and updates CHANGELOG with a bug fix for OTLP
    Trace Exporters to Event Exporter.
    > 
    > - **Release 0.9.1**:
    > - Update `Sources/LaunchDarklyObservability/Version.swift`
    `sdkVersion` to `0.9.1`.
    >   - Update `.release-please-manifest.json` to `0.9.1`.
    > - Add `CHANGELOG.md` entry noting bug fix: OTLP Trace Exporters to
    Event Exporter.
    > 
    > <sup>Written by [Cursor
    Bugbot](https://cursor.com/dashboard?tab=bugbot) for commit
    d383ddb51c1c375cf47b2a3a460303be2f890878. This will update automatically
    on new commits. Configure
    [here](https://cursor.com/dashboard?tab=bugbot).</sup>
    <!-- /CURSOR_SUMMARY -->

--- Sources/LaunchDarklyObservability/Version.swift
+++ Sources/LaunchDarklyObservability/Version.swift
@@ -1,2 +1,2 @@
 // This file is automatically updated by release-please. Do not edit manually.
-public let sdkVersion = "0.9.0" // x-release-please-version
+public let sdkVersion = "0.9.1" // x-release-please-version
