86c18452fbca852f4617b040e0e3a06176d3eb1f
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-11-05 09:02:14 -0800

    refactor to TraceEventExporter

---
+++ Sources/Observability/Traces/SpanItem.swift
@@ -0,0 +1,20 @@
+import Foundation
+import OpenTelemetrySdk
+
+struct SpanItem: EventQueueItemPayload {
+    var exporterClass: AnyClass {
+        Observability.OtlpTraceEventExporter.self
+    }
+    
+    let spanData: SpanData
+    var timestamp: TimeInterval
+    
+    init(spanData: SpanData) {
+        self.spanData = spanData
+        self.timestamp = spanData.endTime.timeIntervalSince1970
+    }
+    
+    public func cost() -> Int {
+        300 + spanData.events.count * 100 + spanData.attributes.count * 100
+    }
+}
