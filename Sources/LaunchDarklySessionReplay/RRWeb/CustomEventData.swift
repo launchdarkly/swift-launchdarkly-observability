b6cbed2d40f55a61704b4aa62ef8d8527c772733
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-12-11 13:03:10 -0800

    delete commented code

--- Sources/LaunchDarklySessionReplay/RRWeb/CustomEventData.swift
+++ Sources/LaunchDarklySessionReplay/RRWeb/CustomEventData.swift
@@ -20,25 +20,3 @@ struct ClickPayload: Codable {
     var clickTextContent: String
     var clickSelector: String
 }
-
-struct IdentityPayload: Codable {
-//    var userIdentifier: String
-//    var telemetrySdkName: String
-//    var telemetrySdkVersion: String
-//    var featureFlagSetId: String
-//    var featureFlagProviderName: String
-    var user: String
-    var key: String
- //   var canonicalKey: String
-    
-    enum CodingKeys: String, CodingKey {
-//        case userIdentifier
-//        case telemetrySdkName = "telemetry.sdk.name"
-//        case telemetrySdkVersion = "telemetry.sdk.version"
-//        case featureFlagSetId = "feature_flag.set.id"
-//        case featureFlagProviderName = "feature_flag.provider.name"
-        case user
-        case key
-//        case canonicalKey
-    }
-}
