d8ca003ae3b471179dd5698689dd7d70c6699995
mario-launchdarkly <mcanto@launchdarkly.com>
2025-12-15 10:03:03 -0600

    chore: remove disableErrorTracking option

--- Sources/LaunchDarklyObservability/API/Options.swift
+++ Sources/LaunchDarklyObservability/API/Options.swift
@@ -12,7 +12,6 @@ import OSLog
 ///   - customHeaders Custom headers to include with OTLP exports.
 ///   - sessionBackgroundTimeout Session timeout if app is backgrounded. Defaults to 15 minutes. 15 * 60
 ///   - isDebug Enables verbose telemetry logging if true as well as other debug functionality. Defaults to false.
-///   - disableErrorTracking Disables error tracking if true. Defaults to false.
 ///   - disableLogs Disables logs if true. Defaults to false.
 ///   - disableTraces Disables traces if true. Defaults to false.
 ///   - disableMetrics Disables metrics if true. Defaults to false.
@@ -186,7 +185,6 @@ public struct Options {
     public var urlBlocklist: [String]
     public var sessionBackgroundTimeout: TimeInterval
     public var isDebug: Bool
-    public var disableErrorTracking: Bool
     public var logsApiLevel: LogLevel
     public var metricsApi: AppMetrics
     public var tracesApi: AppTracing
@@ -207,7 +205,6 @@ public struct Options {
         urlBlocklist: [String] = [],
         sessionBackgroundTimeout: TimeInterval = 15 * 60,
         isDebug: Bool = false,
-        disableErrorTracking: Bool = false,
         logsApiLevel: LogLevel = .info,
         tracesApi: AppTracing = .enabled,
         metricsApi: AppMetrics = .enabled,
@@ -226,7 +223,6 @@ public struct Options {
         self.urlBlocklist = urlBlocklist
         self.sessionBackgroundTimeout = sessionBackgroundTimeout
         self.isDebug = isDebug
-        self.disableErrorTracking = disableErrorTracking
         self.logsApiLevel = logsApiLevel
         self.tracesApi = tracesApi
         self.metricsApi = metricsApi
