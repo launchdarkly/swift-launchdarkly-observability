c6220805a879bfc2c7645138793ad46d9c33cef9
LaunchDarklyReleaseBot <86431345+LaunchDarklyReleaseBot@users.noreply.github.com>
2026-01-09 01:34:29 -0500

    chore(main): release 0.15.1

--- Sources/LaunchDarklyObservability/Version.swift
+++ Sources/LaunchDarklyObservability/Version.swift
@@ -1,2 +1,2 @@
 // This file is automatically updated by release-please. Do not edit manually.
-public let sdkVersion = "0.15.0" // x-release-please-version
+public let sdkVersion = "0.15.1" // x-release-please-version
