f0a0bb12c66de60cd9df4fe18c98a8488651fc38
Andrey Belonogov <abelonogov@launchdarkly.com>
2026-03-13 10:02:01 -0700

    use in664

--- Sources/LaunchDarklyObservability/Bridge/AttributeConverter.swift
+++ Sources/LaunchDarklyObservability/Bridge/AttributeConverter.swift
@@ -58,7 +58,7 @@ enum AttributeConverter {
             case "d", "f":
                 return .double(nsNum.doubleValue)
             default:
-                return .int(nsNum.intValue)
+                return .int(nsNum.integerValue)
             }
         }
 
